bb86aa20549b8bbbfacd9d7c4a587024311d1431
LeGion <mehrdad.dadashi@gmail.com>
2022-11-18 16:46:06 +0300

    write api_client.dart class

---
+++ lib/data/api/api_client.dart
@@ -0,0 +1,17 @@
+/*5:15 fo tutorial */
+import 'package:get/get.dart';
+
+class ApiClient extends GetConnect implements GetxService {
+  late String token;
+  late Map<String, String> _mainHeaders;
+  final String appBaseUrl;
+
+  ApiClient({required this.appBaseUrl}) {
+    baseUrl = appBaseUrl;
+    timeout = const Duration(seconds: 30);
+    _mainHeaders = {
+      'Content-type': 'application/json;charset=UTF-8',
+      'Authorization': 'Bearer $token'
+    };
+  }
+}
